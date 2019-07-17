f84694b6719c15ba5a5ace73203dad521f4bc02f
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-07-17 22:59:24 +0300

    Created a AutoGameSystem initialization attribute

---
+++ Runtime/Attributes/AutoGameSystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hirame.Pantheon
+{
+    [AttributeUsage (AttributeTargets.Class, AllowMultiple = false, Inherited =  false)]
+    public class AutoGameSystem : Attribute
+    {
+        internal static List<Type> GameSystemTypes = new List<Type> ();
+        
+        public AutoGameSystem (Type systemType)
+        {
+            GameSystemTypes.Add (systemType);
+        }
+    }
+
+}
