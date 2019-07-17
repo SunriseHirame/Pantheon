2df1af5df6571e5f52761214a1ff221b4cf5c66c
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-07-17 22:59:04 +0300

    Added a LifeCycleOnUpdate event proxy

---
+++ Runtime/LifeCycle/LifeCycleOnUpdate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hirame.Pantheon
+{
+    public class LifeCycleOnUpdate : LifeCycleEventBase
+    {
+        [SerializeField, Min (0)] private float spacing = 0.5f;
+
+        private float nextPlayTime;
+
+        private void OnEnable ()
+        {
+            nextPlayTime = Time.time + delay;
+        }
+
+        // TODO:
+        // Replace with a lighter recurrency model
+        private void Update ()
+        {
+            var time = Time.time;
+
+            if (time < nextPlayTime)
+                return;
+            
+            nextPlayTime = time + spacing;
+            @event.Invoke ();
+        }
+    }
+}
