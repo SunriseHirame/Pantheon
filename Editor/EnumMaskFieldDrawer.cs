f9be2e4ff5a391595b2affacdd8543b4c218146b
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-07-20 23:13:27 +0300

    Added EnumMaskField drawer
    This will probably move into its own package later

---
+++ Editor/EnumMaskFieldDrawer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Hirame.Pantheon.Editor
+{
+    [CustomPropertyDrawer (typeof (MaskFieldAttribute))]
+    public class EnumMaskFieldDrawer : PropertyDrawer
+    {
+        public override void OnGUI (Rect position, SerializedProperty property, GUIContent label)
+        {
+            using (var scope = new EditorGUI.ChangeCheckScope ())
+            {
+                var value = (uint) (EditorGUI.MaskField (position, label, property.intValue, property.enumNames));
+                if (scope.changed)
+                    property.intValue = (int) value;
+            }
+        }
+    }
+}
---
+++ Runtime/Attributes/MaskFieldAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hirame.Pantheon
+{
+    [AttributeUsage (AttributeTargets.Field)]
+    public sealed class MaskFieldAttribute : PropertyAttribute
+    {
+        
+    }
+
+}
