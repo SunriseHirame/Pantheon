5a79268b444f3e600d8daba22a85ec58d3450009
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-07-24 22:41:23 +0300

    Implemented a basic fixedcapacity Stack

---
+++ Runtime/Types/FastStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Hirame.Pantheon
+{
+    public class FastStack<T>
+    {
+        private T[] items;
+        private int itemCount;
+        private int capacity;
+
+        public FastStack (int startCapacity)
+        {
+            capacity = startCapacity;
+        }
+
+        public void Push (T item)
+        {
+            if (itemCount == capacity)
+                throw new IndexOutOfRangeException ("Capacity reached");
+            
+            items[itemCount++] = item;
+        }
+
+        public T Pop ()
+        {
+            return itemCount == 0 ? default : items[itemCount--];
+        }
+
+        public void Resize (int newCapacity)
+        {
+            Array.Resize (ref items, newCapacity);
+        }
+    }
+
+}
