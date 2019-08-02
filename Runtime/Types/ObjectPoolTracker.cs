e0fffef819f433cc36620219915c830c62522bd2
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-08-02 04:26:50 +0300

    Created a object pool tracker component

---
+++ Runtime/Types/ObjectPoolTracker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hirame.Pantheon
+{
+    public sealed class ObjectPoolTracker<T> : MonoBehaviour where T : Component
+    {
+        public GameObjectPool<T> Pool;
+        public T TrackedComponent;
+
+        public void SetUp (T trackedComponent, GameObjectPool<T> pool)
+        {
+            Pool = pool;
+            TrackedComponent = trackedComponent;
+        }
+        
+        public void ReturnToPool ()
+        {
+            if (Pool == null || TrackedComponent == false)
+                return;
+            
+            Pool.AddItem (TrackedComponent);
+        } 
+
+        public void ReturnToPool (float delay)
+        {
+            if (delay > 0)
+                StartCoroutine (ReturnToPoolAfterDelay (delay));
+            else
+                ReturnToPool ();
+        }
+
+        internal IEnumerator ReturnToPoolAfterDelay (float delay)
+        {
+            yield return new WaitForSeconds (delay);
+            ReturnToPool ();
+        }
+    }
+
+}
