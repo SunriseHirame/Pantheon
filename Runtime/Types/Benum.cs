56bce843ab9386eb39fbe75123d12131d85fed45
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-08-29 01:44:52 +0300

    Added basic test benum

---
+++ Runtime/Types/Benum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Hirame.Pantheon
+{
+    [StructLayout (LayoutKind.Explicit)]
+    [Serializable]
+    public struct Benum
+    {
+        [FieldOffset (0)]
+        [SerializeField] private int integerValue;
+        
+        [FieldOffset (0)]
+        private Enum enumValue;
+
+        public Benum (Enum value)
+        {
+            integerValue = 0;
+            enumValue = value;
+        }
+    }
+
+}
