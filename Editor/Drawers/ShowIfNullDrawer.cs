7d6fd0724c3de7c42ebc0efb90ccdfcb0855c6db
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-11-04 04:42:53 +0200

    Created a ShowIfNull property attribute

---
+++ Editor/Drawers/ShowIfNullDrawer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Hirame.Pantheon.Editor
+{
+    [CustomPropertyDrawer (typeof (ShowIfNull))]
+    public class ShowIfNullDrawer : PropertyDrawer
+    {
+        public override void OnGUI (Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.objectReferenceValue != null)
+                return;
+
+            EditorGUI.PropertyField (position, property, label, true);
+        }
+
+        public override float GetPropertyHeight (SerializedProperty property, GUIContent label)
+        {
+            return property.objectReferenceValue == null ? base.GetPropertyHeight (property, label) : 0f;
+        }
+    }
+
+}
---
+++ Runtime/Attributes/ShowIfNull.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+namespace Hirame.Pantheon
+{
+    [Conditional ("UNITY_EDITOR")]
+    public class ShowIfNull : PropertyAttribute
+    {
+        
+    }
+}
