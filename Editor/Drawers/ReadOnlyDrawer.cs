3ebf614af222f6f96d22db5950071e5c21f9eba7
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-11-08 03:15:28 +0200

    Created a ReadOnly Drawer and Attribute

---
+++ Editor/Drawers/ReadOnlyDrawer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Hirame.Pantheon.Editor
+{
+    [CustomPropertyDrawer (typeof (ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : DecoratorDrawer
+    {
+        public override void OnGUI (Rect position)
+        {
+            GUI.enabled = false;
+        }
+
+        public override float GetHeight ()
+        {
+            return 0f;
+        }
+
+//        public override void OnGUI (Rect position, SerializedProperty property, GUIContent label)
+//        {
+//            var guiEnabled = GUI.enabled;
+//            GUI.enabled = false;
+//            EditorGUI.PropertyField (position, property, label);
+//            GUI.enabled = guiEnabled;
+//        }
+//
+//        public override float GetPropertyHeight (SerializedProperty property, GUIContent label)
+//        {
+//            return EditorGUI.GetPropertyHeight (property, label, true);
+//        }
+    }
+
+}
---
+++ Runtime/Attributes/ReadOnlyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace Hirame.Pantheon
+{
+    [AttributeUsage (AttributeTargets.Field)]
+    [Conditional ("UNITY_EDITOR")]
+    public class ReadOnlyAttribute : PropertyAttribute
+    {
+        
+    }
+
+}
