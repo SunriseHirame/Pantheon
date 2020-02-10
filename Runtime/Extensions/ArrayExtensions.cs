5f59a717ee83191c482a5f0c985b6c7bbbcfeaf6
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2020-02-11 01:42:25 +0200

    Added GetRandom to Arrays

---
+++ Runtime/Extensions/ArrayExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Hirame.Pantheon
+{
+    public static class ArrayExtensions
+    {
+        public static T GetRandom<T> (this T[] array)
+        {
+            Assert.IsTrue (array != null);
+            Assert.IsTrue (array.Length > 0);
+            
+            return array[Random.Range (0, array.Length - 1)];
+        }
+    }
+
+}
