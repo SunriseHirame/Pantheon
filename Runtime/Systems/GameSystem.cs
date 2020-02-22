1ad2aa75afe9ed360f68894465cefde400abdc88
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2020-02-22 04:03:09 +0200

    Added GameSystem OnCreated and OnDisposed

--- Runtime/Systems/GameSystem.cs
+++ Runtime/Systems/GameSystem.cs
@@ -3,22 +3,36 @@
     public abstract class GameSystem<T> : GameSystem where T : GameSystem<T>, new ()
     {
         private static T instance;
+
+        public bool IsActive { get; private set; } = true;
         
         public static T GetOrCreate ()
         {
-            if (instance != null)
-                return instance;
+            return instance ?? CreateNewInstance ();
+        }
+
+        private static T CreateNewInstance ()
+        {
+            var newInstance = new T ();
             
-            instance = new T ();
-            
-            if (instance is IUpdate updateable)
+            if (newInstance is IUpdate updateable)
                 UpdateLoop.RegisterForUpdate (updateable);
-            
-            return instance;
+
+            instance.OnCreated ();
+            return newInstance;
+        }
+        
+        public static void Dispose ()
+        {
+            instance.OnDisposed ();
+            instance = null;
         }
     }
 
     public abstract class GameSystem
     {
+        protected virtual void OnCreated () { }
+        
+        protected virtual void OnDisposed () { }
     }
 }
