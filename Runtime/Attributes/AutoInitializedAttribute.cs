7cf8fa3e9ceae324c015b688e89077968ead40ee
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2020-04-05 20:09:58 +0300

    Updated auto initialization attribute with ininitialization timing parameter

--- Runtime/Attributes/AutoInitializedAttribute.cs
+++ Runtime/Attributes/AutoInitializedAttribute.cs
@@ -1,8 +1,15 @@
-﻿namespace Hirame.Pantheon
+﻿using UnityEngine;
+
+namespace Hirame.Pantheon
 {
     public class AutoInitializedAttribute : System.Attribute
     {
+        public RuntimeInitializeLoadType InitializeLoadType { get; private set; }
         
+        public AutoInitializedAttribute (RuntimeInitializeLoadType initializeLoadType)
+        {
+            InitializeLoadType = initializeLoadType;
+        }
     }
 
 }
